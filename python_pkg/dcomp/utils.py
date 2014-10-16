c43e9ac7c68d1ecfdf7734ad6028addcf0655c68
danithaca <danithaca@gmail.com>
2014-10-16 09:00:36 -0400

    add python package

---
+++ python_pkg/dcomp/utils.py
@@ -0,0 +1,25 @@
+__author__ = 'Daniel Zhou'
+
+
+class DConfig():
+  def __init__(self):
+    self.properties = {}
+
+
+class DDrush():
+
+  def __init__(self, drush_command, site_alias):
+    self.drush_command = drush_command
+    self.site_alias = site_alias
+
+
+
+
+def check_python_version():
+  import sys
+  return sys.version_info[0] >= 3 or (sys.version_info[0] >= 2 and sys.version_info[1] >=7)
+
+
+def get_agent_name():
+  import socket
+  return socket.gethostname()
