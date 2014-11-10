7c12ed2005f6a851f9f5b08e10a46f3fc07815e3
Daniel Zhou (TechBrewery) <danithaca@gmail.com>
2014-11-10 08:47:50 -0500

    rename {computing} to {computing_record}

--- javasrc/org/drupal/project/computing/DDatabase.java
+++ javasrc/org/drupal/project/computing/DDatabase.java
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
  *
  * Due to security reasons, it is not recommended to use this class directly to access Drupal database unless necessary.
  * Typical reasons to use this class: 1) performance, 2) prototyping. Other than those 2 cases, you should consider
- * use {computing} to pass data in/out of Drupal.
+ * use {computing_record} to pass data in/out of Drupal.
  *
  * It is also recommended to save database credentials in Drupal settings.php, and then use $GLOBALS['databases']
  * to get the credentials and pass to agent using a computing record.
